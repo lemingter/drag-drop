cdaa568c49d298582cbc35ab9d967656d8dac678
lemingter <leetv2.1@gmail.com>
2022-07-08 12:56:24 -0500

    Boards ready

---
+++ src/Components/Lane/Lane.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Lane.css"
+
+function Lane({title}) {
+    return (
+        <div className="Lane-wrapper">
+            <h2>{title}</h2>
+        </div>
+    );
+}
+
+export default Lane;
