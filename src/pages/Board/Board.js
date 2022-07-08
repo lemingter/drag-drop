efcf17af12637c9e83645df11ee4b992ccc6a1be
lemingter <leetv2.1@gmail.com>
2022-07-08 13:26:04 -0500

    Json ready

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lane from '../../Components/Lane/Lane';
 import './Board.css';
 
@@ -10,11 +10,40 @@ const lanes = [
 ]
 
 function Board(props) {
+    const [loading, setLoading] = useState(false);
+    const [tasks, setTask] = useState([]);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const task = await fetch('https://my-json-server.typicode.com/lemingter/myAPI/tasks');
+                const result = await task.json();
+
+                if(result) {
+                    setTask(result);
+                    setLoading(false);
+                }
+            } catch (e) {
+                setLoading(false);
+                setError(e.error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className="Board-wrapper">
             {
                 lanes.map((lane) => (
-                    <Lane key={lane.id} title={lane.title}/>
+                    <Lane 
+                        key={lane.id} 
+                        title={lane.title}
+                        loading = {loading}
+                        error={error}
+                        tasks={tasks.filter((task) => task.lane === lane.id)}
+                    />
                 ))
             }
         </div>
